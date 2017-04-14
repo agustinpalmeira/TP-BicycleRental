fdb2bd8faf46cb749dfdc86f317d74d8c8a6dbcb
anarella <anarellanicoletta@hotmail.com>
2017-04-13 23:47:59 -0300

    Add files via upload
    Script que permite reducir el dataset 'status.csv' (de 72 millones de registros a 2 millones). Genera el archivo 'status_red.csv'

---
+++ modificacion_status.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import csv
+
+def main():
+	counter = 0
+	with open("status.csv", "r") as f:
+		status = csv.reader(f)
+		with open("status_red.csv", "w") as d:
+			status_red = csv.writer(d)
+			status_red.writerow(["station_id", "bikes_available", "docks_available", "start_time", "end_time"])
+
+			station_id_previous = ""
+			bikes_available_previous = ""
+			docks_available_previous = ""
+			start_time_previous = ""
+			end_time_previous = ""
+
+			primer_registro = True
+			status.next()
+
+			for station_id, bikes_available, docks_available, time in status:
+
+				#counter = counter + 1
+
+				if primer_registro == True:
+					station_id_previous = station_id
+					bikes_available_previous = bikes_available
+					docks_available_previous = docks_available
+					start_time_previous = time
+					end_time_previous = time
+					primer_registro = False
+				else:
+					if (station_id_previous == station_id and bikes_available_previous == bikes_available and docks_available_previous == docks_available):
+						end_time_previous = time
+					else:
+						status_red.writerow([station_id_previous, bikes_available_previous, docks_available_previous, start_time_previous, end_time_previous])
+						station_id_previous = station_id
+						bikes_available_previous = bikes_available
+						docks_available_previous = docks_available
+						start_time_previous = time
+						end_time_previous = time
+				#if counter == 70:
+					#break
+
+			status_red.writerow([station_id_previous, bikes_available_previous, docks_available_previous, start_time_previous, end_time_previous])
+					
+	#print counter
+
+main()
