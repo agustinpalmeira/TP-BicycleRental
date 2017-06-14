dc3f27c1c8af254fca6ad80dd7a11f88a6619bc4
bmsanchez <sanchezbarbaramariana@gmail.com>
2017-06-14 12:17:59 -0300

    Add files via upload

---
+++ TP2/etc.py
@@ -0,0 +1,29 @@
+import csv
+import random
+import scipy
+import operator
+import unicodedata
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import ExtraTreesRegressor
+
+train = pd.read_csv("trainEditadoDist.csv", header=0, \
+                      delimiter=",", quoting=2)
+labels = train['duration']
+train = train.drop(['duration'],1)
+etc= ExtraTreesRegressor()
+et = etc.fit(train, labels)
+test= pd.read_csv("testEditado7.csv", header=0,delimiter=",", quoting=2)
+test2=test
+test = test.drop(['id'],1)
+etc_preds = etc.predict(test)
+output = pd.DataFrame( data={"id":test2["id"], "duration":etc_preds} )
+#output.to_csv( "submission45.csv", index=False, quoting=1 )
+
+y=output
+y['id'] = y['id'].astype(int)
+y['duration']= y['duration'] .astype(int)
+aux= pd.read_csv("trip.csv", header=0,delimiter=",", quoting=2)
+prueba = pd.merge(aux,y,how='inner', on='id')
+MSE = mean_squared_error(prueba['duration_x'],prueba['duration_y'])
+print "error cuadratico medio",MSE
--- TP2/rf_regressor.py
+++ TP2/rf_regressor.py
@@ -8,20 +8,28 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 
-train = pd.read_csv("trainEditado3.csv", header=0, \
+train = pd.read_csv("trainEditadoDist.csv", header=0, \
                       delimiter=",", quoting=2)
 labels = train['duration']
 train = train.drop(['duration'],1)
-#X_train, X_test, y_train, y_test = train_test_split(train, labels, test_size=0.4, random_state = 42)
+X_train, X_test, y_train, y_test = train_test_split(train, labels, test_size=0.4, random_state = 42)
 
-rfr = RandomForestRegressor(n_estimators=100)
-#rf = rfr.fit(X_train, y_train)
+rfr = RandomForestRegressor(n_estimators=150)
+#rf = rfr.fit(X_train,y_train)
 rf= rfr.fit(train, labels)
 #print rfr.score(X_test, y_test)
 
-test= pd.read_csv("testEditado3.csv", header=0,delimiter=",", quoting=2)
+test= pd.read_csv("testEditado7.csv", header=0,delimiter=",", quoting=2)
 test2=test
 test = test.drop(['id'],1)
 rfr_preds = rfr.predict(test)
-output = pd.DataFrame( data={"Id":test2["id"], "duration":rfr_preds} )
-output.to_csv( "submission25.csv", index=False, quoting=1 )
+output = pd.DataFrame( data={"id":test2["id"], "duration":rfr_preds} )
+
+#output.to_csv( "submission44.csv", index=False, quoting=1 )
+y=output
+y['id'] = y['id'].astype(int)
+y['duration']= y['duration'] .astype(int)
+aux= pd.read_csv("trip.csv", header=0,delimiter=",", quoting=2)
+prueba = pd.merge(aux,y,how='inner', on='id')
+MSE = mean_squared_error(prueba['duration_x'],prueba['duration_y'])
+print "error cuadratico medio",MSE
