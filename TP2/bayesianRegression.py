c6edfe82e4c169b2fd02bd159613be53594aa935
Agustin Palmeira <agustin.palmeira@fdvsolutions.com>
2017-06-14 11:19:19 -0300

    - Perceptron & Bayesian algorithms modified & their outputs

--- TP2/bayesianRegression.py
+++ TP2/bayesianRegression.py
@@ -14,13 +14,46 @@ import pandas as pd
 #reg.predict ([[1, 0.]])
 #reg.coef_
 
-train = pd.read_csv("trainEditado.csv", header=0, delimiter=",", quoting=2)
+
+print('Reading Train...')
+
+train = pd.read_csv("/Users/agustinpalmeira/Documents/Agus Varios/Facu/datos/tp2017/trainEditado.csv", header=0, delimiter=",", quoting=2)
 labels = train['duration']
 train = train.drop(['duration'], 1)
 X_train, X_test, y_train, y_test = train_test_split(train, labels, test_size=0.4, random_state = 42)
 
+print('Generating Bayesian...')
+
 regr = linear_model.BayesianRidge()
+
+print('Fitting train...')
 
 re = regr.fit(X_train, y_train)
 
+print('...')
+
+print('Accurancy:')
+
 print regr.score(X_test, y_test)
+
+print('Reading test CSV...')
+
+test = pd.read_csv("/Users/agustinpalmeira/Documents/Agus Varios/Facu/datos/tp2017/testEditado.csv", header=0, delimiter=",", quoting=2)
+
+print('Test CSV read...')
+
+test2 = test
+
+print ('Drop...')
+
+test = test.drop(['id'], 1)
+
+bayesianPredictor = re.predict(test)
+
+print('Generating Data Frame output...')
+
+output = pd.DataFrame(data={"Id": test2["id"], "duration": bayesianPredictor})
+
+print('Generating CSV output...')
+
+output.to_csv("submissionBayesian.csv", index=False, quoting=1)
--- TP2/perceptron.py
+++ TP2/perceptron.py
@@ -4,17 +4,38 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Perceptron
 import pandas as pd
 
-train = pd.read_csv("trainEditado.csv", header=0, delimiter=",", quoting=2)
+train = pd.read_csv("/Users/agustinpalmeira/Documents/Agus Varios/Facu/datos/tp2017/trainEditado.csv", header=0, delimiter=",", quoting=2)
 print('Read csv')
 labels = train['duration']
 train = train.drop(['duration'], 1)
-X_train, X_test, y_train, y_test = train_test_split(train, labels, test_size=0.4, random_state = 42)
+X_train, X_test, y_train, y_test = train_test_split(train, labels, test_size=0.2, random_state = 20)
 
-print('Begin Train')
+print('Trainning...')
 
 clf = Perceptron()
 clf.fit(X_train, y_train)
 
-print ('Finished Train')
+print ('Finished Train...')
 
 print('Accuracy:', clf.score(X_test, y_test))
+
+print('Reading test CSV...')
+
+test = pd.read_csv("/Users/agustinpalmeira/Documents/Agus Varios/Facu/datos/tp2017/testEditado.csv", header=0, delimiter=",", quoting=2)
+
+print('Test CSV read...')
+
+test2 = test
+test = test.drop(['id'], 1)
+
+print('...')
+
+perceptronPredictor = clf.predict(test)
+
+print('Generating output...')
+
+output = pd.DataFrame(data={"Id": test2["id"], "duration": perceptronPredictor})
+
+print('...')
+
+output.to_csv("submissionsarasa.csv", index=False, quoting=1)
