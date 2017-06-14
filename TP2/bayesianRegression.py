cab290f5a800568d5c7ee6b1ca40713e3580cb40
Agustin Palmeira <agustin.palmeira@fdvsolutions.com>
2017-06-14 20:29:23 -0300

    - Quadratic error added to the Bayesian Regression

--- TP2/bayesianRegression.py
+++ TP2/bayesianRegression.py
@@ -1,7 +1,7 @@
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 import pandas as pd
-
+from sklearn.metrics import mean_squared_error
 
 #http://scikit-learn.org/stable/modules/linear_model.html#bayesian-regression
 
@@ -17,7 +17,7 @@ import pandas as pd
 
 print('Reading Train...')
 
-train = pd.read_csv("/Users/agustinpalmeira/Documents/Agus Varios/Facu/datos/tp2017/trainEditado.csv", header=0, delimiter=",", quoting=2)
+train = pd.read_csv("trainEditado.csv", header=0, delimiter=",", quoting=2)
 labels = train['duration']
 train = train.drop(['duration'], 1)
 X_train, X_test, y_train, y_test = train_test_split(train, labels, test_size=0.4, random_state = 42)
@@ -38,7 +38,7 @@ print regr.score(X_test, y_test)
 
 print('Reading test CSV...')
 
-test = pd.read_csv("/Users/agustinpalmeira/Documents/Agus Varios/Facu/datos/tp2017/testEditado.csv", header=0, delimiter=",", quoting=2)
+test = pd.read_csv("testEditado7.csv", header=0, delimiter=",", quoting=2)
 
 print('Test CSV read...')
 
@@ -56,4 +56,21 @@ output = pd.DataFrame(data={"Id": test2["id"], "duration": bayesianPredictor})
 
 print('Generating CSV output...')
 
-output.to_csv("submissionBayesian.csv", index=False, quoting=1)
+output.to_csv("submissionBayesian2.csv", index=False, quoting=1)
+
+y = output
+y['id'] = y['Id'].astype(int)
+y['duration'] = y['duration'] .astype(int)
+aux = pd.read_csv("trip.csv", header=0,delimiter=",", quoting=2)
+
+print('Merging...')
+
+prueba = pd.merge(aux, y, how='inner', on='id')
+
+print('Calculating Cuadratic Error...')
+
+MSE = mean_squared_error(prueba['duration_x'], prueba['duration_y'])
+
+print "Error cuadratico medio: ", MSE
+
+#Error cuadratico medio:  43963182.7828
